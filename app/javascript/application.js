39ea73b7747a1c7d4a39cd02fc9526fe73abc502
Rodrigo Fernandes <1895242+rodnandes@users.noreply.github.com>
2024-09-19 21:55:19 -0300

    Add comments section to list

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -16,7 +16,18 @@ createApp(
     methods: {
       async fetchStories() {
         this.stories = await (await fetch('api/stories.json')).json()
+      },
+
+      toggleComments(_event, storyId) {
+        const story = this.findStory(storyId)
+        story.commentsOpened = !story.commentsOpened
+        story.commentToggleButtonText = story.commentsOpened ? '-Hide' : '+Show'
+      },
+
+      findStory(storyId) {
+        return this.stories.find(story => story.hn_id === storyId)
       }
+
     }
   }
 ).mount('#vue-app')
