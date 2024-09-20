337e1fa0f66960c00feda75f00f6b5c82b9ecada
Rodrigo Fernandes <1895242+rodnandes@users.noreply.github.com>
2024-09-20 12:37:35 -0300

    Fix Date formatting

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -42,7 +42,14 @@ createApp(
       },
 
       formatDate(date) {
-        return new Date(date).toLocaleString('pt-BR', {timeStyle: 'short'})
+        return new Date(date).toLocaleString('pt-BR', {
+          year: 'numeric',
+          month: '2-digit',
+          day: '2-digit',
+          hour: '2-digit',
+          minute: '2-digit',
+          hour12: false,
+        })
       }
 
     }
